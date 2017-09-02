9f92c73cc704d7d16e30490f60ad0b41bff6a46b
Elevista <bjlee@bpnr.co.kr>
2017-09-03 04:00:37 +0900

    eslint standard

--- MariaDB.js
+++ MariaDB.js
@@ -1,7 +1,7 @@
 const {sails} = global
 const co = require('co')
 const mysql = require('mysql')
-const connInfo = sails && sails.config.MariaDBInfo || sails.config.connections.MariaDBInfo  // eslint-disable-line no-mixed-operators
+const connInfo = sails && (sails.config.MariaDBInfo || sails.config.connections.MariaDBInfo)
 if (!connInfo) return
 
 const connectionPool = mysql.createPool(connInfo)
