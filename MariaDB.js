7de4c116ecd923785b4824b9fc30dfa02352c395
Elevista <sunnyholic@sunnyholic.com>
2017-12-20 17:21:16 +0900

    Restructure v1.1.0

--- MariaDB.js
+++ MariaDB.js
@@ -10,7 +10,7 @@ const type = {':': '?', ';': '??'}
 const re = /([:;$#])([a-zA-Z_]+[a-zA-Z0-9_]*)/g
 
 function parseSql (str, params) {
-  if (!params) return [str, []]
+  if (!(params instanceof Object)) return [str, []]
   let values = []
   let sql = (str || '').replace(re, function (m, $1, $2) {
     if ($1 === '$') return params[$2] ? mysql.escape(params[$2]) : ''
@@ -21,34 +21,50 @@ function parseSql (str, params) {
   return [sql, values]
 }
 
-function promisify (fn, that) {
-  return function (...args) {
-    return new Promise((resolve, reject) => fn.apply(that, [...args, (err, result) => err ? reject(err) : resolve(result)]))
-  }
+function promisify (that, fn) {
+  return (...args) => new Promise((resolve, reject) =>
+    fn.apply(that, [...args, (err, result) => err ? reject(err) : resolve(result)])
+  )
 }
 
 function promisifyConnection (connection) {
-  return {
-    query (sql, params) {
-      let [sqlString, values] = parseSql(sql, params)
-      return promisify(connection.query, connection)(sqlString, values).catch(error => Promise.reject(new Error([sql, params, error])))
-    },
-    beginTransaction: promisify(connection.beginTransaction, connection),
-    commit: promisify(connection.commit, connection),
-    rollback: promisify(connection.rollback, connection),
-    release: () => connection.release()
-  }
+  if (connection.hasOwnProperty('prototype')) return connection
+  let {beginTransaction, commit, rollback, query, changeUser, on, ping, release, destroy} = connection
+  let _ = fn => promisify(connection, fn)
+  let that = fn => (...args) => fn.apply(connection, args)
+
+  let _query = _(query)
+  return Object.assign(Object.create(connection, {prototype: {value: connection}}), {
+    _promisify: _,
+    _bind: that,
+    query: (sql, params) => _query(...parseSql(sql, params)).catch(error => Promise.reject(new Error([sql, params, error]))),
+    beginTransaction: _(beginTransaction),
+    commit: _(commit),
+    rollback: _(rollback),
+    changeUser: _(changeUser),
+    on: _(on),
+    ping: _(ping),
+    release: that(release),
+    destroy: that(destroy)
+  })
+  // NOTE: If want to call that not own method of this object,
+  // please use 'conn.prototype.foo(...args)'
 }
 
-module.exports = function (gen) {
+function isPromise (obj) { // from 'co'
+  return obj && typeof obj.then === 'function'
+}
+
+function MariaDB (gen) {
   return co(function * () {
-    let connection = yield promisify(connectionPool.getConnection, connectionPool)()
-    yield promisify(connection.beginTransaction, connection)()
-    return promisifyConnection(connection)
+    let connection = yield promisify(connectionPool, connectionPool.getConnection)()
+    let conn = promisifyConnection(connection)
+    yield conn.beginTransaction()
+    return conn
   }).then(conn => co(function * () {
     try {
       let res = yield * gen(conn)
-      if (res instanceof Promise) res = yield res
+      if (isPromise(res)) res = yield res
       yield conn.commit()
       conn.release()
       return res
@@ -60,3 +76,8 @@ module.exports = function (gen) {
     }
   }))
 }
+
+module.exports = MariaDB
+module.exports.MariaDB = MariaDB
+module.exports.MariaDBInfo = connInfo
+module.exports.mysql = mysql
